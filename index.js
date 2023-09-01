8df92d9edc76ce2029b5c250456df11495f2f109
Miculino <128690939+Miculino@users.noreply.github.com>
2023-09-01 13:57:01 +0200

    add cors

--- index.js
+++ index.js
@@ -7,10 +7,7 @@ require("dotenv").config();
 
 const app = express()
 
-// app.use(cors({
-//   origin: 'http://localhost:3000',
-//   optionsSuccessStatus: 200
-// }));
+app.use(cors());
 
 app.use(bodyParser.urlencoded({
   extended: true
